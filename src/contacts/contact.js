d0f249bed1f5f0a392e943533047d2072c9cd6f1
saran2653 <saran26053@gmail.com>
2024-05-22 09:20:11 +0530

    contact form

--- src/contacts/contact.js
+++ src/contacts/contact.js
@@ -1,13 +1,29 @@
-import "./contact.css"
-function Contact(){
-return(
+import "./contact.css";
+function Contact() {
+  return (
     <div className="contact-container">
-
-        <form>
-            <input type="text"></input>
-            
-        </form>
+        <h className="contact-head">Contact</h>
+      <form name="contact" netlify>
+        <p>
+          <label>
+            Name <input type="text" name="name" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Email <input type="email" name="email" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Message <textarea type="textarea" name="message"></textarea>
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
     </div>
-)
+  );
 }
 export default Contact;

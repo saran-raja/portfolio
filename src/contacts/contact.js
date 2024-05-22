5ad3384a556d5c58d438654e0e89131784bfe57c
saran2653 <saran26053@gmail.com>
2024-05-22 12:42:17 +0530

    added succes action

--- src/contacts/contact.js
+++ src/contacts/contact.js
@@ -1,28 +1,37 @@
-import React from 'react';
+import React from "react";
 import "./contact.css";
 
 function Contact() {
   return (
     <div className="contact-container">
       <h1 className="contact-head">Contact</h1>
-      <form 
-        name="contact" 
-        method="POST" 
-        data-netlify="true" 
+      <form
+        name="contact"
+        method="POST"
+        data-netlify="true"
+        action="/success/"
         data-netlify-honeypot="bot-field"
       >
         <input type="hidden" name="form-name" value="contact" />
-        <p style={{ display: 'none' }}>
-          <label>Don’t fill this out if you're human: <input name="bot-field" /></label>
+        <p style={{ display: "none" }}>
+          <label>
+            Don’t fill this out if you're human: <input name="bot-field" />
+          </label>
         </p>
         <p>
-          <label>Your Name: <input type="text" name="name" required /></label>
+          <label>
+            Your Name: <input type="text" name="name" required />
+          </label>
         </p>
         <p>
-          <label>Your Email: <input type="email" name="email" required /></label>
+          <label>
+            Your Email: <input type="email" name="email" required />
+          </label>
         </p>
         <p>
-          <label>Message: <textarea name="message" required></textarea></label>
+          <label>
+            Message: <textarea name="message" required></textarea>
+          </label>
         </p>
         <p>
           <button type="submit">Send</button>

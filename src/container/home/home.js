f5e052aa032224fe2d114cbfff3e010e3541b8ab
saran2653 <saran26053@gmail.com>
2024-05-22 18:15:41 +0530

    responsive added

--- src/container/home/home.js
+++ src/container/home/home.js
@@ -11,48 +11,54 @@ import profile from "./saran-profile.jpg";
 function Home() {
   return (
     <div className="home-container">
-      <section className="contact-icon">
-        <a
-          href="https://github.com/yourprofile"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <GithubOutlined />
-        </a>
-        <a
-          href="https://www.linkedin.com/in/saran2653"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <LinkedinOutlined />
-        </a>
-        <a
-          href="mailto:your.email@example.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <MailOutlined />
-        </a>
-      </section>
-      <img src={profile} alt="profile" className="profile" />
-      <section className="text-content">
-        <h1>Hello,</h1>
-        <h2>
-          I'm <span className="highlight">Saran</span>
-        </h2>
-        <h3>Full stack developer</h3>
-        <p>
-          "I'm a proficient web designer, deeply committed to crafting visually
-          stunning and user-friendly websites."
-        </p>
-        <Button
-          className="custom-button"
-          onClick={() => (window.location.href = "mailto:saran26053@gmail.com")}
-          icon={<MailOutlined />}
-        >
-          Contact Me
-        </Button>
-      </section>
+      <div>
+        <section className="contact-icon">
+          <a
+            href="https://github.com/yourprofile"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <GithubOutlined />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/saran2653"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <LinkedinOutlined />
+          </a>
+          <a
+            href="mailto:your.email@example.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <MailOutlined />
+          </a>
+        </section>
+        <section className="text-content">
+          <h1>Hello,</h1>
+          <h2>
+            I'm <span className="highlight">Saran</span>
+          </h2>
+          <h3>Full stack developer</h3>
+          <p>
+            "I'm a proficient web designer, deeply committed to crafting
+            visually stunning and user-friendly websites."
+          </p>
+          <Button
+            className="custom-button"
+            onClick={() =>
+              (window.location.href = "mailto:saran26053@gmail.com")
+            }
+            icon={<MailOutlined />}
+          >
+            Contact Me
+          </Button>
+        </section>
+      </div>
+      <image className="profile-container">
+        <img src={profile} alt="profile" className="profile" />
+      </image>
     </div>
   );
 }

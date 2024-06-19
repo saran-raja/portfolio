ad5bfe765f70795e91966a38a6d1a1f2c7cbe91c
saran2653 <saran26053@gmail.com>
2024-06-19 20:40:00 +0530

    added weather app in project

--- src/container/project/project.js
+++ src/container/project/project.js
@@ -1,48 +1,36 @@
-import { Carousel } from "antd";
+import { GithubOutlined } from "@ant-design/icons";
 import "./project.css";
-import product1s1 from "./project1.png";
-import product1s2 from "./p1s2.png";
-import product1s3 from "./p1s3.png";
-import product1s4 from "./p1s4.png";
-import product1s5 from "./p1s5.png";
+import weather from "./weather-app.png";
+
 function Project() {
-  const contentStyle = {
-    margin: 0,
-    height: "300px",
-    color: "#fff",
-    textAlign: "center",
-    background: "#364d79",
-  };
   return (
     <div className="project-container">
       <section className="project-head">
-        <h>Project</h>
-        <p>What i have done</p>
-        <h>E-COMMERCE</h>
+        <h1>Projects</h1>
+        <p>What I have done</p>{" "}
       </section>
-      <section className="project-slider">
-        <div className="project">
-          <Carousel arrows infinite={false} className="slider">
-            <div>
-              <img src={product1s1} style={contentStyle} alt="project"></img>
-            </div>
-            <div>
-            <img src={product1s2} style={contentStyle} alt="project"></img>
-            </div>
-            <div>
-            <img src={product1s3} style={contentStyle} alt="project"></img>
-            </div>
-            <div>
-            <img src={product1s4} style={contentStyle} alt="project"></img>
-            </div>
-            <div>
-            <img src={product1s5} style={contentStyle} alt="project"></img>
-            </div>
-            
-          </Carousel>
-          <br />{" "}
-        </div>
-      </section>
+      <div className="weather-app-container">
+        <section className="weather-app">
+          <img src={weather} alt="Weather App" className="weather-image" />
+          <p className="weather-description">
+            A real-time weather website built with React, displaying current
+            conditions from external APIs with a responsive design.
+          </p>
+          <ul className="language-description">
+            <li style={{ color: "#61DBFB" }}>#react</li>
+            <li style={{ color: "#E34C26" }}>#html</li>
+            <li style={{ color: "#F0DB4F" }}>#javascript</li>
+            <li style={{ color: "#264de4" }}>#css</li>
+            <li style={{ color: "#7C3AED" }}>#api</li>
+          </ul>
+          <div className="project-links">
+            <a href={"https://weather-saran.netlify.app/"}>
+              <button>Visit</button>
+            </a>
+            <GithubOutlined />
+          </div>
+        </section>
+      </div>
     </div>
   );
 }

44fde48a6574e78adf179d844533fe246aa75a3e
saran2653 <saran26053@gmail.com>
2024-06-19 21:59:10 +0530

    added e commerce project

--- src/container/project/project.js
+++ src/container/project/project.js
@@ -1,15 +1,18 @@
 import { GithubOutlined } from "@ant-design/icons";
 import "./project.css";
 import weather from "./weather-app.png";
+import ecommerce from "./e-commerce-app.png";
+
 
 function Project() {
   return (
-    <div className="project-container">
+    <div className="projects\
+    -container">
       <section className="project-head">
         <h1>Projects</h1>
         <p>What I have done</p>{" "}
       </section>
-      <div className="weather-app-container">
+      <div className="project-container">
         <section className="weather-app">
           <img src={weather} alt="Weather App" className="weather-image" />
           <p className="weather-description">
@@ -17,16 +20,36 @@ function Project() {
             conditions from external APIs with a responsive design.
           </p>
           <ul className="language-description">
-            <li style={{ color: "#61DBFB" }}>#react</li>
-            <li style={{ color: "#E34C26" }}>#html</li>
-            <li style={{ color: "#F0DB4F" }}>#javascript</li>
-            <li style={{ color: "#264de4" }}>#css</li>
-            <li style={{ color: "#7C3AED" }}>#api</li>
+          <li className="react">#react</li>
+            <li className="html">#html</li>
+            <li className="javascript">#javascript</li>
+            <li className="css">#css</li>
+            <li className="api">#api</li>
           </ul>
           <div className="project-links">
             <a href={"https://weather-saran.netlify.app/"}>
               <button>Visit</button>
             </a>
+            <GithubOutlined />
+          </div>
+        </section>
+        <section className="ecommerce-app">
+          <img src={ecommerce} alt="Weather App" className="weather-image" />
+          <p className="weather-description">
+          <p>User-friendly e-commerce platform, built with React and styled with Ant Design for a seamless shopping experience.</p>
+          </p>
+          <ul className="language-description">
+            <li className="react">#react</li>
+            <li className="html">#html</li>
+            <li className="javascript">#javascript</li>
+            <li className="css">#css</li>
+            <li className="api">#api</li>
+            <li className="ant-design">#ant design</li>
+          </ul>
+          <div className="ecommerce-project-links">
+            {/* <a href={"https://weather-saran.netlify.app/"}> */}
+              <button>  Ongoing</button>
+            {/* </a> */}
             <GithubOutlined />
           </div>
         </section>
